a68b5a1322bdcb2efda7b23b69b43a4536be441e
Brian Nkhata <bnkhata@mra.mw>
2024-06-24 16:24:27 +0200

    complete login functionality

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    # login user
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
 
-# def login_user(request):
-#     pass
+        # authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged In")
+            return redirect('home')
+        else:
+            messages.success(request, "Error logging In")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
 
 def logout_user(request):
     pass
